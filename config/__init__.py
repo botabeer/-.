8301fcad151848ca2480730ee01c1295102234f9
عبير <alqaws151@gmail.com>
2025-11-16 18:07:16 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,40 +1,6 @@
 """
-Config Package
-Contains all configuration and settings for the LINE Bot
+استيراد الإعدادات لجعلها متاحة على مستوى المجلد
 """
+from .config import config, BotConfig, THEME, NO_POINTS_GAMES
 
-from .settings import (
-    LINE_TOKEN,
-    LINE_SECRET,
-    GEMINI_KEYS,
-    GEMINI_MODEL,
-    COLORS,
-    RATE_LIMIT
-)
-
-from .database import (
-    init_db,
-    update_points,
-    get_stats,
-    get_leaderboard
-)
-
-from .helpers import (
-    normalize_text,
-    load_file
-)
-
-__all__ = [
-    'LINE_TOKEN',
-    'LINE_SECRET',
-    'GEMINI_KEYS',
-    'GEMINI_MODEL',
-    'COLORS',
-    'RATE_LIMIT',
-    'init_db',
-    'update_points',
-    'get_stats',
-    'get_leaderboard',
-    'normalize_text',
-    'load_file'
-]
+__all__ = ['config', 'BotConfig', 'THEME', 'NO_POINTS_GAMES']
