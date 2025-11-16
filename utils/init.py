c8108f985daf9ef344321e4ec3121d03c6171765
عبير <alqaws151@gmail.com>
2025-11-16 18:11:59 +0300

    Create init.py

---
+++ utils/init.py
@@ -0,0 +1,6 @@
+"""
+استيراد الدوال المساعدة
+"""
+from .utils import safe_text, normalize_text, load_file, get_profile_safe, check_rate
+
+__all__ = ['safe_text', 'normalize_text', 'load_file', 'get_profile_safe', 'check_rate']
