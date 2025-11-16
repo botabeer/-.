a3dd46b20d9c5b3248d9c1ee7867e8130979e267
عبير <alqaws151@gmail.com>
2025-11-16 19:04:32 +0300

    Update __init__.py

--- cache/__init__.py
+++ cache/__init__.py
@@ -1,3 +1,8 @@
-from .cache import CacheManager
+from .cache_manager import CacheManager
 
-__all__ = ["CacheManager"]
+# إنشاء الـ singleton objects للاستخدام في المشروع
+names_cache = CacheManager()
+stats_cache = CacheManager()
+leaderboard_cache = CacheManager()
+
+__all__ = ['names_cache', 'stats_cache', 'leaderboard_cache', 'CacheManager']
