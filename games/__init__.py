d855cadf08f84d0c3532b466bb6534f7ead27a53
عبير <alqaws151@gmail.com>
2025-11-17 00:39:52 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,19 +1,19 @@
 from .song_game import SongGame
-from .opposite_game import OppositeGame
-from .compatibility_game import CompatibilityGame
-from .differences_game import DifferencesGame
-from .fast_typing_game import FastTypingGame
-from .chain_words_game import ChainWordsGame
 from .human_animal_plant_game import HumanAnimalPlantGame
+from .chain_words_game import ChainWordsGame
+from .fast_typing_game import FastTypingGame
+from .opposite_game import OppositeGame
 from .letters_words_game import LettersWordsGame
+from .differences_game import DifferencesGame
+from .compatibility_game import CompatibilityGame
 
 __all__ = [
-    'SongGame',
-    'OppositeGame',
-    'CompatibilityGame',
-    'DifferencesGame',
-    'FastTypingGame',
-    'ChainWordsGame',
-    'HumanAnimalPlantGame',
-    'LettersWordsGame'
+    "SongGame",
+    "HumanAnimalPlantGame",
+    "ChainWordsGame",
+    "FastTypingGame",
+    "OppositeGame",
+    "LettersWordsGame",
+    "DifferencesGame",
+    "CompatibilityGame"
 ]
