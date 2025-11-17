bb4597f507776a9684ac85010c2ff83ca9cd8a83
عبير <alqaws151@gmail.com>
2025-11-17 23:15:57 +0300

    Delete cache directory

--- cache/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cache_manager import CacheManager
-
-# إنشاء الـ singleton objects للاستخدام في المشروع
-names_cache = CacheManager()
-stats_cache = CacheManager()
-leaderboard_cache = CacheManager()
-
-__all__ = ['names_cache', 'stats_cache', 'leaderboard_cache', 'CacheManager']
