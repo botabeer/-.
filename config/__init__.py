caad822ec740307527fd5d9fa339e3322e03b80b
عبير <alqaws151@gmail.com>
2025-11-17 23:16:04 +0300

    Delete config directory

--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-استيراد الإعدادات لجعلها متاحة على مستوى المجلد
-"""
-from .config import config, BotConfig, THEME, NO_POINTS_GAMES
-
-__all__ = ['config', 'BotConfig', 'THEME', 'NO_POINTS_GAMES']
