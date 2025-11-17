d005457524bc72e034bd96722e4193f4991c455b
عبير <alqaws151@gmail.com>
2025-11-17 23:16:12 +0300

    Delete database directory

--- database/__init__.py
+++
@@ -1,5 +0,0 @@
-from .db import Database
-from .models import Player, GameLog
-from .db_manager import db_manager
-
-__all__ = ['Database', 'Player', 'GameLog', 'db_manager']
--- database/db_manager.py
+++
@@ -1,4 +0,0 @@
-from .db import Database
-
-# إنشاء كائن قاعدة بيانات واحد يمكن استدعاؤه في كل المشروع
-db_manager = Database("game_bot.db")
