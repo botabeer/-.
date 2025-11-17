9de4ad98807276c43e35f95b7667cdb6905961bc
عبير <alqaws151@gmail.com>
2025-11-17 23:16:54 +0300

    Delete ‏managers directory

--- ‏managers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-استيراد المديرين
-"""
-from .user_manager import UserManager
-from .game_manager import GameManager
-from .cleanup_manager import cleanup_manager
-
-__all__ = ['UserManager', 'GameManager', 'cleanup_manager']
