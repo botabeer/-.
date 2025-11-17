bd983a1774eb7f62af7a23676f4f9f92c8a4f8c0
عبير <alqaws151@gmail.com>
2025-11-17 23:16:39 +0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-استيراد الدوال المساعدة
-"""
-from .utils import safe_text, normalize_text, load_file, get_profile_safe, check_rate
-
-__all__ = ['safe_text', 'normalize_text', 'load_file', 'get_profile_safe', 'check_rate']
