4181b77a6e1a2c09cdfcc2b936aec1dded1171d5
عبير <alqaws151@gmail.com>
2025-11-19 15:50:44 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,8 +1,36 @@
-# games/__init__.py
+"""
+مجلد الألعاب - Whale Bot Games Package
+يحتوي على جميع الألعاب المتاحة في البوت
+"""
 
-# استيراد الدالة start_game من ملف games.py الرئيسي
-try:
-    from .games import start_game
-except ImportError:
-    start_game = None
-    print("✗ تحذير: لم يتم تحميل start_game من games.py")
+from .game_opposite import OppositeGame
+from .game_song import SongGame
+from .game_chain import ChainWordsGame
+from .game_order import OrderGame
+from .game_build import BuildGame
+from .game_lbgame import LBGame
+from .game_fast import FastGame
+from .game_compatibility import CompatibilityGame
+
+__all__ = [
+    'OppositeGame',
+    'SongGame',
+    'ChainWordsGame',
+    'OrderGame',
+    'BuildGame',
+    'LBGame',
+    'FastGame',
+    'CompatibilityGame'
+]
+
+# قاموس جميع الألعاب المتاحة
+GAME_CLASSES = {
+    'ضد': OppositeGame,
+    'اغنية': SongGame,
+    'سلسلة': ChainWordsGame,
+    'ترتيب': OrderGame,
+    'تكوين': BuildGame,
+    'لعبة': LBGame,
+    'اسرع': FastGame,
+    'توافق': CompatibilityGame
+}
