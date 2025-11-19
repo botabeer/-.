a37e4155b1bc68cf75b1440b1c4025782c0a5124
عبير <alqaws151@gmail.com>
2025-11-20 00:34:59 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,36 +1,40 @@
 """
 مجلد الألعاب - Whale Bot Games Package
-يحتوي على جميع الألعاب المتاحة في البوت
+جميع الألعاب المتاحة في البوت
 """
 
-from .game_opposite import OppositeGame
-from .game_song import SongGame
-from .game_chain import ChainWordsGame
-from .game_order import OrderGame
-from .game_build import BuildGame
-from .game_lbgame import LBGame
-from .game_fast import FastGame
-from .game_compatibility import CompatibilityGame
+from .speed import SpeedGame
+from .word_game import WordGame
+from .chain_words import ChainWordsGame
+from .song import SongGame
+from .opposite import OppositeGame
+from .order import OrderGame
+from .build import BuildGame
+from .compatibility import CompatibilityGame
 
 __all__ = [
-    'OppositeGame',
-    'SongGame',
+    'SpeedGame',
+    'WordGame',
     'ChainWordsGame',
+    'SongGame',
+    'OppositeGame',
     'OrderGame',
     'BuildGame',
-    'LBGame',
-    'FastGame',
     'CompatibilityGame'
+    'AiGame'
 ]
 
-# قاموس جميع الألعاب المتاحة
+# قاموس الألعاب المتاحة
 GAME_CLASSES = {
-    'ضد': OppositeGame,
-    'اغنية': SongGame,
+    'اسرع': SpeedGame,
+    'لعبة': WordGame,
     'سلسلة': ChainWordsGame,
+    'اغنية': SongGame,
+    'ضد': OppositeGame,
     'ترتيب': OrderGame,
     'تكوين': BuildGame,
-    'لعبة': LBGame,
-    'اسرع': FastGame,
     'توافق': CompatibilityGame
 }
+
+def get_game_instance(game_type):
+    """إرجاع instance من اللعبة المط
